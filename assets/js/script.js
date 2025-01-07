a1da6343cfa4d4478d5d13c94ab17cc0decabb98
Shaun Walker <mybluewebsite@dontsp.am>
2025-01-07 20:58:16 +0000

    added board option function for player and computer and added some HTML and CSS style

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,12 +17,23 @@ function rotate() {
 // Battleboard function
 const width = 10;
 
-function createBattleboard() {
+function createBattleboard(color, user) {
     const battleboardOption = document.createElement("div");
     battleboardOption.classList.add("battleboard");
-    battleboardOption.style.backgroundColor = "lightgreen";
+    battleboardOption.style.backgroundColor = color;
+    battleboardOption.id = user;
+
+    for (let i = 0; i < width * width; i++) {
+        const block = document.createElement("div");
+        block.classList.add("block");
+        block.id = i;
+        battleboardOption.append(block)
+    };
+
+    battleboardOptions.append(battleboardOption);
 };
 
-
+createBattleboard("blue", "player")
+createBattleboard("navy", "computer")
 
 rotateButton.addEventListener("click", rotate);
