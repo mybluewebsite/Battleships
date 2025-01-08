75ad63dd06c427f462652721db243d8ad2d6cc99
Shaun Walker <mybluewebsite@dontsp.am>
2025-01-08 20:14:06 +0000

    added more functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const rotateButton = document.getElementById("rotate-button");
 
 // Rotation button function
 let angle = 0;
+
 function rotate() {
     const battleshipOptions = Array.from(options.children);
     if (angle === 0) {
@@ -56,28 +57,44 @@ const frigate = new ship("Frigate", 3);
 const ships = [destroyer, submarine, carrier, minehunter, frigate];
 
 function addShip(ship) {
-    const allShipBlocks = document.querySelectorAll("computer div");
+    const allBoardBlocks = document.querySelectorAll("computer div");
     let randomBoolean = Math.random() < 0.5;
     let isHorizontal = randomBoolean;
     let randomStart = Math.floor(Math.random() * width * width);
+
+    let validStart = isHorizontal ? randomStart <= width * width - ship.length ? randomStart :
+        width * width - ship.length : randomStart <= width * width - width * ship.length ? randomStart : randomStart - ship.length * width + width
 
     let shipBlocks = [];
 
     for (let i = 0; i < ship.length; i++) {
         if (isHorizontal) {
-            shipBlocks.push(allShipBlocks[Number(randomStart) + i]);
+            shipBlocks.push(allBoardBlocks[Number(validStart) + i]);
         } else {
-            shipBlocks.push(allShipBlocks[Number(randomStart) = i * width]);
+            shipBlocks.push(allBoardBlocks[Number(validStart) + i * width]);
         };
     };
 
-    shipBlocks.forEach(shipBlock => {
-        shipBlock.classList.add(ship.name);
-        shipBlock.classList.add("taken");
-    });
+    let valid
 
+    if (isHorizontal) {
+        valid = shipBlocks.every((_shipBlock, index) => shipBlocks[0].id % width !== width - (shipBlocks.length - (index + 1)))
+    } else {
+        valid = shipBlocks.every((_shipBlock, index) => shipBlocks[0].id < 90 + (width * index + 1))
+    }
+
+    const notTaken = shipBlocks.every(shipBlock => !shipBlock.classList.contains("taken"))
+
+
+    if (valid && notTaken) {
+        shipBlocks.forEach(shipBlock => {
+            shipBlock.classList.add(ship.name)
+            shipBlock.classList.add("taken")
+        });
+    } else {
+        addShip(ship)
+    }
 };
-
 
 
 ships.forEach(ship => addShip(ship));
