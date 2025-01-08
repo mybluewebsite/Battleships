00d567bd97f16f3544517ed4eb7cbfdbd83b1384
Shaun Walker <mybluewebsite@dontsp.am>
2025-01-08 07:23:21 +0000

    added ship creation function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,8 @@ function rotate() {
     battleshipOptions.forEach(battleshipOption => battleshipOption.style.transform = `rotate(${angle}deg)`);
 };
 
+rotateButton.addEventListener("click", rotate);
+
 // Battleboard creation function
 const width = 10;
 
@@ -36,4 +38,44 @@ function createBattleboard(color, user) {
 createBattleboard("blue", "player")
 createBattleboard("navy", "computer")
 
-rotateButton.addEventListener("click", rotate);
+// Ship creation function
+
+class ship {
+    constructor(name, length) {
+        this.name = name;
+        this.length = length;
+    };
+};
+
+const destroyer = new ship("Destroyer", 4);
+const submarine = new ship("Submarine", 3);
+const carrier = new ship("Carrier", 5);
+const minehunter = new ship("Minehunter", 2);
+const frigate = new ship("Frigate", 3);
+
+const ships = [destroyer, submarine, carrier, minehunter, frigate];
+
+function addShip(ship) {
+    const allShipBlocks = document.querySelectorAll("computer div");
+    let randomBoolean = math.random() < 0.5;
+    let isHorizontal = randomBoolean;
+    let randomStart = Math.floor(Math.random() * width * width);
+
+    let shipBlocks = [];
+
+    for (let i = 0; i < ship.length; i++) {
+        if (isHorizontal) {
+            shipBlocks.push(allShipBlocks[Number(randomStart) + i]);
+        } else {
+            shipBlocks.push(allShipBlocks[Number(randomStart) = i * width]);
+        };
+    };
+};
+
+shipBlocks.forEach(shipBlock => {
+    shipBlock.classList.add(ship.name);
+    shipBlock.classList.add("taken");
+});
+
+
+addShip(ships);
